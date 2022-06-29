3f4f55282ebd293533deca5d589bb5059c22b514
SusanBird <susanbird391@gmail.com>
2022-06-28 17:05:43 -0700

    sign in user test written

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -24,6 +24,17 @@ describe('users', () => {
   });
 });
 
+it('signs in an existing user', async () => {
+  await UserService.create(mockUser);
+  const { email, password } = mockUser;
+  const res = await request(app)
+    .post('/api/v1/users/sessions')
+    .send({ email, password });
+  console.log(res.body);
+  expect(res.status).toBe(200);
+  expect(res.body).toEqual({ message: 'Signed in successfully!' });
+});
+
 
 afterAll(() => {
   pool.end();
